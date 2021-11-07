a01cb18c8b67076c0fa803b434f078880bd840bd
0bs72 <92104414+0bs72@users.noreply.github.com>
2021-11-07 13:30:48 +0000

    Update bip0039.py

--- bip0039.py
+++ bip0039.py
@@ -21,7 +21,7 @@ def format_str(value):
 
 # The 2048 english word list is hosted at: https://github.com/bitcoin/bips/blob/master/bip-0039/english.txt
 # Download into the same local directory as this script
-# Use the cli commenad: wget https://github.com/bitcoin/bips/raw/master/bip-0039/english.txt
+# Use the cli command: wget https://github.com/bitcoin/bips/raw/master/bip-0039/english.txt
 # Advice: make the local file 'read only'
 
 def get_word(word):
